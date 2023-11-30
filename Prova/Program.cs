b9cc2a4d9c18bf0c6d57b118184444ed1ab36fe4
Gabriel <gabrielteles931@gmail.com>
2023-11-30 15:34:51 -0300

    primeiro commit

--- Prova/Program.cs
+++ Prova/Program.cs
@@ -1,3 +1,9 @@
 ﻿class Advogado{
-    private string Nome,DataNascimento,Cpf;
+    private string Nome,DataNascimento,CPF,CNA;
+
+    
+}
+
+class Cliente{
+    private string Nome,DataNascimento,CPF,EstadoCivil,Profissao;
 }
