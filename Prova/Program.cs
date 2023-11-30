1735c8d8fb7695da62bc26f526700fe172e8e0af
Gabriel <gabrielteles931@gmail.com>
2023-11-30 15:51:51 -0300

    fazendo validação do CPF no modelo xxx.xxx.xxx.-xx

--- Prova/Program.cs
+++ Prova/Program.cs
@@ -1,7 +1,29 @@
 ﻿class Advogado{
     private string Nome,DataNascimento,CPF,CNA;
 
-    
+    public CPF{
+        get{
+            return (CPF)base.
+        }
+        set{
+        do
+        {
+        Console.WriteLine("Informe o seu CPF: ");
+        string _cpf = Console.ReadLine();
+             
+        bool ValidarFormatoCPF(string cpf){
+        string pattern = @"^\d{3}\.\d{3}\.\d{3}-\d{2}$";
+        return Regex.IsMatch(cpf, pattern);
+        }
+        if(ValidarFormatoCPF(_cpf) == true{
+            CPF = _cpf
+        }else{
+            Console.WriteLine("CPF inválido Informe um cpf no padão xxx.xxx.xxx.-xx");
+        }    
+        } while (verifFormat == false)
+        }
+    }
+
 }
 
 class Cliente{
