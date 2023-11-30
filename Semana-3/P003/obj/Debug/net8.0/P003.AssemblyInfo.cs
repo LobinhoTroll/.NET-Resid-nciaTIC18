e98050efd8fd84138502d73f09f704441375e01d
LobinhoTroll <gabrielteles931@gmail.com>
2023-11-30 13:38:30 -0300

    coisas

--- Semana-3/P003/obj/Debug/net8.0/P003.AssemblyInfo.cs
+++ Semana-3/P003/obj/Debug/net8.0/P003.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("P002-1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("P003")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("P002-1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("P002-1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d05a1d2dd09c14a58b455986db415604960f2c16")]
+[assembly: System.Reflection.AssemblyProductAttribute("P003")]
+[assembly: System.Reflection.AssemblyTitleAttribute("P003")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
